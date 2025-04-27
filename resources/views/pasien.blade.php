1ef5eb7f6c7ee41ee5062bd95d90e2e554c7cdad
feri irawan <feri909192@gmail.com>
2025-04-28 00:53:49 +0700

    update

--- resources/views/pasien.blade.php
+++ resources/views/pasien.blade.php
@@ -123,7 +123,7 @@
                 <div class="container-fluid px-4">
                     <h1 class="mt-4">Pasien</h1>
                     <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item">Pasien</li>
+                        <li class="breadcrumb-item active">Pasien</li>
                     </ol>
                     <div class="row">
                         <div class="col-xl-3 col-md-6">
--- resources/views/rekam-medis.blade.php
+++ resources/views/rekam-medis.blade.php
@@ -123,7 +123,7 @@
                 <div class="container-fluid px-4">
                     <h1 class="mt-4">Rekam Medis</h1>
                     <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item">Rekam Medis</li>
+                        <li class="breadcrumb-item active">Rekam Medis</li>
                     </ol>
                     <div class="row">
                         <div class="col-xl-3 col-md-6">
