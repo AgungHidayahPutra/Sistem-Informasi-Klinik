ec961005f1eb6dc72b41864160e6157b36d17f77
AgungHidayahPutra <agunghidayahputra@gmail.com>
2025-04-27 22:04:49 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('/dokter', function () {
+    return view('dokter');
+});
+
+Route::get('/antrian', function () {
+    return view('antrian');
+});
+
+Route::get('/jadwal', function () {
+    return view('jadwal');
+});
+
+Route::get('/pasien', function () {
+    return view('pasien');
+});
+
+Route::get('/pembayaran', function () {
+    return view('pembayaran');
+});
+
+Route::get('/rekam-medis', function () {
+    return view('rekam-medis');
+});
