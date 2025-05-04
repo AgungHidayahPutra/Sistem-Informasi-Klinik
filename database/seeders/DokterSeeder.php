5d2f3598e5e0a87429175f82f413ac0a6a6bca0d
feri irawan <feri909192@gmail.com>
2025-05-05 02:39:01 +0700

    bahan data dokter

--- database/seeders/DokterSeeder.php
+++ database/seeders/DokterSeeder.php
@@ -38,5 +38,65 @@ class DokterSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+
+        Dokter::create([
+            'nama_dokter' => 'dr. Dedi',
+            'spesialis' => 'Umum',
+            'no_hp' => '083312345678',
+            'email' => 'Dedi@gmail.com',
+            'sts_dokter' => 'tersedia',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Dokter::create([
+            'nama_dokter' => 'dr. Eka',
+            'spesialis' => 'Anak',
+            'no_hp' => '084412345678',
+            'email' => 'Eka@gmail.com',
+            'sts_dokter' => 'tidak tersedia',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Dokter::create([
+            'nama_dokter' => 'dr. Fajar',
+            'spesialis' => 'Gigi',
+            'no_hp' => '085512345678',
+            'email' => 'Fajar@gmail.com',
+            'sts_dokter' => 'tersedia',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Dokter::create([
+            'nama_dokter' => 'dr. Gita',
+            'spesialis' => 'Umum',
+            'no_hp' => '086612345678',
+            'email' => 'Gita@gmail.com',
+            'sts_dokter' => 'tersedia',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Dokter::create([
+            'nama_dokter' => 'dr. Hendra',
+            'spesialis' => 'Umum',
+            'no_hp' => '087712345678',
+            'email' => 'Hendra@gmail.com',
+            'sts_dokter' => 'tidak tersedia',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Dokter::create([
+            'nama_dokter' => 'dr. Intan',
+            'spesialis' => 'Anak',
+            'no_hp' => '088812345678',
+            'email' => 'Intan@gmail.com',
+            'sts_dokter' => 'tersedia',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
