517fbe69fd2ab9eaee837b0c7e3e61d8f90a4624
AgungHidayahPutra <agunghidayahputra@gmail.com>
2025-05-05 00:39:19 +0700

    update

--- resources/views/dokter/rekam-medis.blade.php
+++ resources/views/dokter/rekam-medis.blade.php
@@ -96,7 +96,7 @@
                 <div class="sb-sidenav-menu">
                     <div class="nav">
                         <div class="sb-sidenav-menu-heading text-lavender">Dokter</div>
-                        <a class="nav-link text-lavender" href="/dasboard">
+                        <a class="nav-link text-lavender" href="/dashboard">
                             <div class="sb-nav-link-icon text-lavender"><i class="fa-solid fa-house"></i></div>
                             Dashboard
                         </a>
